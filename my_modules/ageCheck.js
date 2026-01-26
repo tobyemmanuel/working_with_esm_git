73d9c2cc43a149658d08b06e64922602fe73aacd
Oluwatobi Adelabu <adelabutobi@gmail.com>
2026-01-26 19:22:48 +0100

    implemented agecheck and user info function

---
+++ my_modules/ageCheck.js
@@ -0,0 +1,7 @@
+export function checkAge(age) {
+  if (age >= 18) {
+    return "an adult";
+  } else {
+    return "a minor";
+  }
+}
---
+++ my_modules/userInfo.js
@@ -0,0 +1,3 @@
+export function formatUserInfo(name, age, status) {
+  return `Hello ${name}! You are ${age} years old and you are ${status}.`;
+}
